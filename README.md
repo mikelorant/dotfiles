087fd44ebc2d7d04ea7d159f3042e4e28cf77800
Michael Lorant <michael.lorant@fairfaxmedia.com.au>
2017-01-25 16:22:02 +1100

    Improvements for work setup

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # dotfiles
+
+## Install
+
+/usr/bin/env bash -c "$(curl -sL https://raw.githubusercontent.com/mikelorant/dotfiles/master/install.sh))"
---
+++ install.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+export FRESH_LOCAL_SOURCE=mikelorant/dotfiles
+
+# /usr/bin/xcode-select -p || /usr/bin/xcode-select --install
+# 
+# echo "After Xcode Command Line Developer Tools has installed press enter."
+# read
+
+[ -f $HOME/.ssh/id_rsa ] || ssh-keygen -f $HOME/.ssh/id_rsa -C $(id -un)
+
+[ -f $HOME/.freshrc ] || /usr/bin/env bash <(curl -sL https://get.freshshell.com)
+ln -sf .dotfiles/fresh/freshrc $HOME/.freshrc
+
+which brew || /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+cd $HOME/.dotfiles
+
+sudo -v
+while true; do sudo -n true; sleep 60; kill -0 "$$" | exit; done 2>/dev/null &
+
+brew update
+brew upgrade
+brew tap homebrew/bundle
+brew bundle --verbose
+
+which zsh | sudo tee -a /etc/shells
+chsh -s $(which zsh)
+
+cd $HOME
+bin/fresh
+
+which atom && apm stars --install
