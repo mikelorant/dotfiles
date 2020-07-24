1badc5e3b69c0ffc24bb2de2d6b8394f0da3f6cc
Michael Lorant <michael.lorant@fairfaxmedia.com.au>
2020-07-24 14:36:37 +1000

    Fixes to iterm2 automation

--- iterm2/iterm2.sh
+++ iterm2/iterm2.sh
@@ -1,21 +1,26 @@
 #!/usr/bin/env sh
 
 CMD="/usr/libexec/PlistBuddy -c"
-FILE="~/Library/Preferences/com.googlecode.iterm2.plist"
+FILE="${HOME}/Library/Preferences/com.googlecode.iterm2.plist"
+
+GUID=$( ${CMD} "print :'New Bookmarks':0:Guid" ${FILE} )
 
 # Allow sessions to survive logging out and back in.
 # This breaks the “auth sufficient pam_tid.so” hack some people use to allow sudo to authenticate with Touch ID.
-defaults write com.googlecode.iterm2 bootstrapDaemon -bool false
+defaults write com.googlecode.iterm2 BootstrapDaemon -bool false
 
 # Disable metal renderer when idle to save CPU utilization.
-defaults write com.googlecode.iterm2 disableMetalWhenIdle -bool true
+defaults write com.googlecode.iterm2 DisableMetalWhenIdle -bool true
 
 # Terminal windows resize smoothly. Disables snapping to character grid.
 # Necessary for applications that manage windows and their sizes such as Rectangle.
-defaults write com.googlecode.iterm2 disableWindowSizeSnap -bool true
+defaults write com.googlecode.iterm2 DisableWindowSizeSnap -bool true
 
 # Automatically check for new versions of iTerm2.
 defaults write com.googlecode.iterm2 SUEnableAutomaticChecks -bool false
+
+# NeverWarnAboutShortLivedSessions
+defaults write com.googlecode.iterm2 NeverWarnAboutShortLivedSessions_${GUID} -bool true
 
 # Use adaptive framerate. When throughput is low, the screen will update at 60 frames per second.
 # When throughput is higher, it will drop to a configurable rate (15 fps by default).
@@ -25,61 +30,83 @@ defaults write com.googlecode.iterm2 SUEnableAutomaticChecks -bool false
 ${CMD} "Add :'New Bookmarks' array" ${FILE}
 
 # Use built-in Powerline glyphs.
+${CMD} "Delete :'New Bookmarks':0:'Draw Powerline Glyphs'" ${FILE}
 ${CMD} "Add :'New Bookmarks':0:'Draw Powerline Glyphs' bool true" ${FILE}
+${CMD} "Delete :'New Bookmarks':0:'Non Ascii Font'" ${FILE}
 ${CMD} "Add :'New Bookmarks':0:'Non Ascii Font' string JetBrainsMonoNerdFontComplete-Regular 12" ${FILE}
+${CMD} "Delete :'New Bookmarks':0:'Normal Font'" ${FILE}
 ${CMD} "Add :'New Bookmarks':0:'Normal Font' string JetBrainsMono-Regular 12" ${FILE}
+# Use Non-ASCII Font
+# The non-ASCII font affects all characters above code point 127.
+${CMD} "Delete :'New Bookmarks':0:'Use Non-ASCII Font'" ${FILE}
+${CMD} "Add :'New Bookmarks':0:'Use Non-ASCII Font' bool true" ${FILE}
+${CMD} "Delete :'New Bookmarks':0:'Show Mark Indicators'" ${FILE}
 ${CMD} "Add :'New Bookmarks':0:'Show Mark Indicators' bool false" ${FILE}
 # Unlimited Scrollback
+${CMD} "Delete :'New Bookmarks':0:'Unlimited Scrollback'" ${FILE}
 ${CMD} "Add :'New Bookmarks':0:'Unlimited Scrollback' bool true" ${FILE}
 # Window Type: (15) Maximized
+${CMD} "Delete :'New Bookmarks':0:'Window Type'" ${FILE}
 ${CMD} "Add :'New Bookmarks':0:'Window Type' integer 15" ${FILE}
 
 # Delete all characters left of the cursor
 # ⌘ + ←Delete
-${CMD} "Add :'New Borkmarks':'Keyboard Maps':0x7f-0x100000:Action integer 11" ${FILE}
-${CMD} "Add :'New Borkmarks':'Keyboard Maps':0x7f-0x100000:Text string 0x15" ${FILE}
+${CMD} "Add :'New Bookmarks':0:'Keyboard Map':0x7f-0x100000:Action integer 11" ${FILE}
+${CMD} "Add :'New Bookmarks':0:'Keyboard Map':0x7f-0x100000:Text string 0x15" ${FILE}
 
 # Delete all characters right of the cursor
 # ⌘ + fn + ←Delete
 # ⌘ + Delete→
-${CMD} "Add :'New Borkmarks':'Keyboard Maps':0xf728-0x100000:Action integer 11" ${FILE}
-${CMD} "Add :'New Borkmarks':'Keyboard Maps':0xf728-0x100000:Text string 0x0b" ${FILE}
+${CMD} "Add :'New Bookmarks':0:'Keyboard Map':0xf728-0x100000:Action integer 11" ${FILE}
+${CMD} "Add :'New Bookmarks':0:'Keyboard Map':0xf728-0x100000:Text string 0x0b" ${FILE}
 
 # Delete one word to left of cursor
 # ⌥ + ←Delete
-${CMD} "Add :'New Borkmarks':'Keyboard Maps':0x7f-0x80000:Action integer 11" ${FILE}
-${CMD} "Add :'New Borkmarks':'Keyboard Maps':0x7f-0x80000:Text string 0x1b 0x08" ${FILE}
+${CMD} "Add :'New Bookmarks':0:'Keyboard Map':0x7f-0x80000:Action integer 11" ${FILE}
+${CMD} "Add :'New Bookmarks':0:'Keyboard Map':0x7f-0x80000:Text string 0x1b 0x08" ${FILE}
 
 # Delete one word to right of cursor
 # ⌥ + fn + ←Delete
 # ⌥ + Delete→
-${CMD} "Add :'New Borkmarks':'Keyboard Maps':0xf728-0x80000:Action integer 11" ${FILE}
-${CMD} "Add :'New Borkmarks':'Keyboard Maps':0xf728-0x80000:Text string 0x1b 0x64" ${FILE}
+${CMD} "Add :'New Bookmarks':0:'Keyboard Map':0xf728-0x80000:Action integer 11" ${FILE}
+${CMD} "Add :'New Bookmarks':0:'Keyboard Map':0xf728-0x80000:Text string 0x1b 0x64" ${FILE}
 
 # Move cursor to the front of line
 # ⌘ + ←
-${CMD} "Add :'New Borkmarks':'Keyboard Maps':0xf702-0x300000:Action integer 11" ${FILE}
-${CMD} "Add :'New Borkmarks':'Keyboard Maps':0xf702-0x300000:Text string 0x01" ${FILE}
+${CMD} "Add :'New Bookmarks':0:'Keyboard Map':0xf702-0x300000:Action integer 11" ${FILE}
+${CMD} "Add :'New Bookmarks':0:'Keyboard Map':0xf702-0x300000:Text string 0x01" ${FILE}
 
 # Move cursor to the end of line
 # ⌘ + →
-${CMD} "Add :'New Borkmarks':'Keyboard Maps':0xf703-0x300000:Action integer 11" ${FILE}
-${CMD} "Add :'New Borkmarks':'Keyboard Maps':0xf703-0x300000:Text string 0x05" ${FILE}
+${CMD} "Add :'New Bookmarks':0:'Keyboard Map':0xf703-0x300000:Action integer 11" ${FILE}
+${CMD} "Add :'New Bookmarks':0:'Keyboard Map':0xf703-0x300000:Text string 0x05" ${FILE}
+
+# Move cursor one word left
+# ⌥ + ←
+${CMD} "Delete :'New Bookmarks':0:'Keyboard Map':0xf702-0x280000" ${FILE}
+${CMD} "Add :'New Bookmarks':0:'Keyboard Map':0xf702-0x280000:Action integer 11" ${FILE}
+${CMD} "Add :'New Bookmarks':0:'Keyboard Map':0xf702-0x280000:Text string 0x1b 0x62" ${FILE}
+
+# Move cursor one word right
+# ⌥ + →
+${CMD} "Delete :'New Bookmarks':0:'Keyboard Map':0xf703-0x280000" ${FILE}
+${CMD} "Add :'New Bookmarks':0:'Keyboard Map':0xf703-0x280000:Action integer 11" ${FILE}
+${CMD} "Add :'New Bookmarks':0:'Keyboard Map':0xf703-0x280000:Text string 0x1b 0x66" ${FILE}
 
 # Undo
 # ⌘ + z
-${CMD} "Add :'New Borkmarks':'Keyboard Maps':0x7a-0x100000:Action integer 11" ${FILE}
-${CMD} "Add :'New Borkmarks':'Keyboard Maps':0x7a-0x100000:Text string 0x1f" ${FILE}
+${CMD} "Add :'New Bookmarks':0:'Keyboard Map':0x7a-0x100000:Action integer 11" ${FILE}
+${CMD} "Add :'New Bookmarks':0:'Keyboard Map':0x7a-0x100000:Text string 0x1f" ${FILE}
 
 # Redo
 # ⇧ + ⌘ + z
-${CMD} "Add :'New Borkmarks':'Keyboard Maps':0x5a-0x120000:Action integer 11" ${FILE}
-${CMD} "Add :'New Borkmarks':'Keyboard Maps':0x5a-0x120000:Text string 0x18 0x1f" ${FILE}
+${CMD} "Add :'New Bookmarks':0:'Keyboard Map':0x5a-0x120000:Action integer 11" ${FILE}
+${CMD} "Add :'New Bookmarks':0:'Keyboard Map':0x5a-0x120000:Text string 0x18 0x1f" ${FILE}
 
 # Redo
 # ⌘ + y
-${CMD} "Add :'New Borkmarks':'Keyboard Maps':0x79-0x100000:Action integer 11" ${FILE}
-${CMD} "Add :'New Borkmarks':'Keyboard Maps':0x79-0x100000:Text string 0x18 0x1f" ${FILE}
+${CMD} "Add :'New Bookmarks':0:'Keyboard Map':0x79-0x100000:Action integer 11" ${FILE}
+${CMD} "Add :'New Bookmarks':0:'Keyboard Map':0x79-0x100000:Text string 0x18 0x1f" ${FILE}
 
 # Show Status Bar
 # Enable this in future revisions.
@@ -87,29 +114,29 @@ ${CMD} "Add :'New Borkmarks':'Keyboard Maps':0x79-0x100000:Text string 0x18 0x1f
 # ${CMD} "Add :'New Bookmarks':0:'Show Status Bar' bool true" ${FILE}
 
 # GlobalTouchBarMap
-${CMD} "Add :GlobalTouchBarMap:touchbarShared:Action integer 12" ${FILE}
-${CMD} "Add :GlobalTouchBarMap:touchbarShared:Label string Shared" ${FILE}
-${CMD} "Add :GlobalTouchBarMap:touchbarShared:Text string kubectl config use-context shared\\\n" ${FILE}
+${CMD} "Add :GlobalTouchBarMap:'touchbar\:shared':Action integer 12" ${FILE}
+${CMD} "Add :GlobalTouchBarMap:'touchbar\:shared':Label string Shared" ${FILE}
+${CMD} "Add :GlobalTouchBarMap:'touchbar\:shared':Text string kubectl config use-context blue-shared\\\n" ${FILE}
 
-${CMD} "Add :GlobalTouchBarMap:touchbarDevelopment:Action integer 12" ${FILE}
-${CMD} "Add :GlobalTouchBarMap:touchbarDevelopment:Label string Development" ${FILE}
-${CMD} "Add :GlobalTouchBarMap:touchbarDevelopment:Text string kubectl config use-context development\\\n" ${FILE}
+${CMD} "Add :GlobalTouchBarMap:'touchbar\:development':Action integer 12" ${FILE}
+${CMD} "Add :GlobalTouchBarMap:'touchbar\:development':Label string Development" ${FILE}
+${CMD} "Add :GlobalTouchBarMap:'touchbar\:development':Text string kubectl config use-context blue-development\\\n" ${FILE}
 
-${CMD} "Add :GlobalTouchBarMap:touchbarProduction:Action integer 12" ${FILE}
-${CMD} "Add :GlobalTouchBarMap:touchbarProduction:Label string Production" ${FILE}
-${CMD} "Add :GlobalTouchBarMap:touchbarProduction:Text string kubectl config use-context production\\\n" ${FILE}
+${CMD} "Add :GlobalTouchBarMap:'touchbar\:production':Action integer 12" ${FILE}
+${CMD} "Add :GlobalTouchBarMap:'touchbar\:production':Label string Production" ${FILE}
+${CMD} "Add :GlobalTouchBarMap:'touchbar\:production':Text string kubectl config use-context blue-production\\\n" ${FILE}
 
-${CMD} "Add :GlobalTouchBarMap:touchbarPlaypen:Action integer 12" ${FILE}
-${CMD} "Add :GlobalTouchBarMap:touchbarPlaypen:Label string Playpen" ${FILE}
-${CMD} "Add :GlobalTouchBarMap:touchbarPlaypen:Text string kubectl config use-context playpen\\\n" ${FILE}
+${CMD} "Add :GlobalTouchBarMap:'touchbar\:playpen':Action integer 12" ${FILE}
+${CMD} "Add :GlobalTouchBarMap:'touchbar\:playpen':Label string Playpen" ${FILE}
+${CMD} "Add :GlobalTouchBarMap:'touchbar\:playpen':Text string kubectl config use-context blue-playpen\\\n" ${FILE}
 
 # NSTouchBarConfig: regular
 ${CMD} "Delete :'NSTouchBarConfig\: regular':CurrentItems" ${FILE}
 ${CMD} "Add :'NSTouchBarConfig\: regular':CurrentItems array" ${FILE}
-${CMD} "Add :'NSTouchBarConfig\: regular':CurrentItems:0 string touchbarShared" ${FILE}
-${CMD} "Add :'NSTouchBarConfig\: regular':CurrentItems:1 string touchbarDevelopment" ${FILE}
-${CMD} "Add :'NSTouchBarConfig\: regular':CurrentItems:2 string touchbarProduction" ${FILE}
-${CMD} "Add :'NSTouchBarConfig\: regular':CurrentItems:3 string touchbarPlaypen" ${FILE}
+${CMD} "Add :'NSTouchBarConfig\: regular':CurrentItems:0 string touchbar:shared" ${FILE}
+${CMD} "Add :'NSTouchBarConfig\: regular':CurrentItems:1 string touchbar:development" ${FILE}
+${CMD} "Add :'NSTouchBarConfig\: regular':CurrentItems:2 string touchbar:production" ${FILE}
+${CMD} "Add :'NSTouchBarConfig\: regular':CurrentItems:3 string touchbar:playpen" ${FILE}
 ${CMD} "Add :'NSTouchBarConfig\: regular':CurrentItems:4 string NSTouchBarItemIdentifierFlexibleSpace" ${FILE}
 ${CMD} "Add :'NSTouchBarConfig\: regular':CurrentItems:5 string iTermTouchBarIdentifierStatus" ${FILE}
 ${CMD} "Add :'NSTouchBarConfig\: regular':CurrentItems:6 string NSTouchBarItemIdentifierOtherItemsProxy" ${FILE}
