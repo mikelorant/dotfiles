57a4ec1316f21caf4e3032ac15471158da69537a
Michael Lorant <michael.lorant@nine.com.au>
2022-12-01 17:09:06 +1100

    [macos] Sort folders before files in finder

--- macos/defaults.sh
+++ macos/defaults.sh
@@ -5,3 +5,5 @@ defaults -currentHost write -glocalDomain CGFontRenderingFontSmoothingDisabled -
 defaults -currentHost write -globalDomain AppleFontSmoothing -int 0
 
 defaults -currentHost write -globalDomain com.apple.swipescrolldirection -bool NO
+
+defaults write com.apple.finder "_FXSortFoldersFirst" -bool "false" && killall Finder
